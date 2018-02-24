2ee80e8fa081c9288b3d3898ad2dc1fae84faeeb
Tarek Ben Taher <tarek.bentaher@gmail.com>
2018-02-24 22:00:16 +0100

    Adding Navigation Bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormioAuthService } from 'angular-formio/auth';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: FormioAuthService) { }
 
   ngOnInit() {
   }
