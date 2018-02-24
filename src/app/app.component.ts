e8e554caa9750736710a482dec3e026fe21b9693
Tarek Ben Taher <tarek.bentaher@gmail.com>
2018-02-24 22:43:47 +0100

    Redirect to Home after login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { FormioAuthService } from 'angular-formio/auth';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private auth: FormioAuthService, private router: Router) {
+    this.auth.onLogin.subscribe(() => {
+      this.router.navigate(['/home']);
+    });
+
+    this.auth.onLogout.subscribe(() => {
+      this.router.navigate(['/auth/login']);
+    });
+
+    this.auth.onRegister.subscribe(() => {
+      this.router.navigate(['/home']);
+    });
+  }
 }
