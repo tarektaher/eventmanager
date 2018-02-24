7dc097a9e1c54f807f1a1c5c20060f9bd1bbd1ec
Tarek Ben Taher <tarek.bentaher@gmail.com>
2018-02-24 21:57:20 +0100

    Include the Auth Service + Auth Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 
+import { FormioAppConfig } from 'angular-formio';
+import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
+import { AuthConfig, AppConfig } from '../config';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -24,10 +28,18 @@ import { HomeComponent } from './home/home.component';
       {
         path: 'home',
         component: HomeComponent
+      },
+      {
+        path: 'auth',
+        loadChildren: () => AuthModule
       }
     ])
   ],
-  providers: [],
+  providers: [
+    FormioAuthService,
+    {provide: FormioAuthConfig, useValue: AuthConfig},
+    {provide: FormioAppConfig, useValue: AppConfig}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
